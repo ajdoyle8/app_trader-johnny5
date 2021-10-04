0e84381453779c0a0319c156a1c70e4a62e1d73e
acgerhold <acgerhold@gmail.com>
2021-10-04 15:52:48 -0500

    mvp final

--- data/app_trader_project.sql
+++ data/app_trader_project.sql
@@ -1,12 +1,32 @@
--- app_store_apps : 7179 rows
---		avg rating : 3.5269556759761012 / 5
--- play_store_apps : 10840 rows
---		avg rating : 4.1917574204569720 / 5
-
-select distinct play_store_apps.name as play_store, play_store_apps.rating as play_store_rating, play_store_apps.price as pp, app_store_apps.name as app_store, app_store_apps.rating as app_store_rating, app_store_apps.price as ap
-from play_store_apps
-left join app_store_apps on play_store_apps.name = app_store_apps.name
-where play_store_apps.name is not null and app_store_apps.name is not null
-order by play_store_rating DESC, app_store_rating DESC
--- 329 rows
-(rating * .5)+1 = years of relevancy * 12 * 5000 = app gross - 1000 = total cost of app
+WITH top_potential_apps AS (
+	SELECT DISTINCT
+		play_store_apps.name AS name,
+		primary_genre,
+		genres,
+		app_store_apps.rating + play_store_apps.rating AS app_rating,
+		10000*(app_store_apps.price::money + play_store_apps.price::money) AS app_price,
+		CASE
+			WHEN 	 10000*(app_store_apps.price::money + play_store_apps.price::money) > 0::money
+				THEN 10000*(app_store_apps.price::money + play_store_apps.price::money)
+			ELSE 10000::money
+		END AS rights,
+	2*(app_store_apps.rating + play_store_apps.rating) + 1 AS life_expectancy_years
+	FROM play_store_apps
+	LEFT JOIN app_store_apps ON play_store_apps.name = app_store_apps.name
+	WHERE play_store_apps.name IS NOT NULL
+		AND app_store_apps.name IS NOT NULL
+	ORDER BY app_rating,
+		app_price DESC
+)
+SELECT name,
+	primary_genre AS App_Store_Genre,
+	genres AS Play_Store_Genre,
+	round(life_expectancy_years/2,2) as life_expectancy,
+	(60000*life_expectancy_years)::money AS expected_revenue,
+	(12000*life_expectancy_years)::money AS marketing_cost,
+	(60000*life_expectancy_years)::money
+		- rights
+		- (12000*life_expectancy_years)::money AS expected_profit
+FROM top_potential_apps
+ORDER BY expected_revenue DESC
+limit 15
